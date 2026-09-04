896077a8a6b6caa614de08f8e3639f0e1e46e2db
lalit7886 <lalitramanmishra@gmail.com>
2026-09-04 22:53:36 +0545

    upto convolution layeres backward pro

--- src/layers/convolution.py
+++ src/layers/convolution.py
@@ -1,29 +1,143 @@
 import numpy as np
-
 class Conv2D:
-    def __init__(self,out_channels=6,in_channels=3,kernel_size=3):
-            self.weights=np.random.randn(out_channels,in_channels,kernel_size,kernel_size)
-            self.bias=np.zeros((out_channels,1))
-            self.out_channels=out_channels
-            self.kernel_size=kernel_size
+    def __init__(self, out_channels=6, in_channels=1, kernel_size=3):
+        self.weights = np.random.randn(
+            out_channels, in_channels, kernel_size, kernel_size
+        )
+        self.bias = np.zeros((out_channels, 1))
 
-    def forward(self,x):
-        "shape of output is (batch, output, filter , filter)"
-        batch_size,channel,height,width=x.shape
-        output=np.zeros((batch_size,self.out_channels,height-self.kernel_size+1,width-self.kernel_size+1))
-        for id,batch in enumerate(x):
-            for out_,filter in enumerate(self.weights):
-                for height_ in range(height-self.kernel_size+1):
-                    for width_ in range(width-self.kernel_size+1):
-                        output[id][out_][height_][width_]=np.sum(filter*batch[:,height_:height_+self.kernel_size,width_:width+self.kernel_size
-                                                                        ])+self.bias[out_][0]
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.input_shape = None
+
+        self.dw_input = None
+        self.dw_bias = None
+        self.dw_weights = None
+
+    def forward(self, x):
+        self.input = x
+        self.input_shape = x.shape
+
+        batch_size, channels, height, width = x.shape
+
+        output_height = height - self.kernel_size + 1
+        output_width = width - self.kernel_size + 1
+
+        output = np.zeros(
+            (batch_size, self.out_channels, output_height, output_width)
+        )
+
+        for batch_index, batch in enumerate(x):
+
+            for filter_id, filter_weights in enumerate(self.weights):
+
+                for h in range(output_height):
+                    for w in range(output_width):
+
+                        input_patch = batch[
+                            :,
+                            h:h + self.kernel_size,
+                            w:w + self.kernel_size
+                        ]
+
+                        output[batch_index, filter_id, h, w] = (
+                            np.sum(filter_weights * input_patch)
+                            + self.bias[filter_id, 0]
+                        )
+
         return output
-    
-    
-    
-    
-                
+
+    def backward(self, d_upstream):
+
+        batch_size, out_channels, output_height, output_width = d_upstream.shape
+
+        _, in_channels, input_height, input_width = self.input_shape
+
+        # Gradients
+        dw_weights = np.zeros_like(self.weights)
+        dw_bias = np.zeros_like(self.bias)
+        dw_input = np.zeros_like(self.input)
+
+        K = self.kernel_size
+
+        for batch_index in range(batch_size):
+
+            for filter_id in range(out_channels):
+
+                # Upstream gradient for this output filter
+                dout_filter = d_upstream[batch_index, filter_id]
+
+                for h in range(output_height):
+                    for w in range(output_width):
+
+                        # Scalar upstream gradient
+                        gradient = dout_filter[h, w]
+
+                        # Corresponding input region
+                        input_patch = self.input[
+                            batch_index,
+                            :,
+                            h:h + K,
+                            w:w + K
+                        ]
+
+                        # Actual CNN filter
+                        filter_weights = self.weights[filter_id]
+
+                        dw_input[
+                            batch_index,
+                            :,
+                            h:h + K,
+                            w:w + K
+                        ] += gradient * filter_weights
+
+
+                        dw_weights[filter_id] += gradient * input_patch
+
             
-        
-        
-        
+                        dw_bias[filter_id, 0] += gradient
+
+        self.dw_input = dw_input
+        self.dw_weights = dw_weights
+        self.dw_bias = dw_bias
+
+        return dw_input, dw_weights, dw_bias
+
+
+if __name__ == "__main__":
+
+    x = np.array([[
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]
+        ]
+    ]], dtype=float)
+
+    conv = Conv2D(
+        out_channels=1,
+        in_channels=1,
+        kernel_size=3
+    )
+
+    output = conv.forward(x)
+
+    print("Input:")
+    print(x)
+
+    print("\nForward output:")
+    print(output)
+
+    d_upstream = np.ones_like(output)
+
+    dx, dw, db = conv.backward(d_upstream)
+
+    print("\ndX:")
+    print(dx)
+
+    print("\ndW:")
+    print(dw)
+
+    print("\ndb:")
+    print(db)
--- src/layers/pooling.py
+++ src/layers/pooling.py
@@ -4,9 +4,12 @@ class average_pooling:
     def __init__(self,pool_size=2,stride=2):
         self.stride=stride
         self.pool=pool_size
+        self.input_shape=None
+        
     
     def forward(self,x:np.array):
-        batch,n_fiters,height,width=x.shape
+        self.input_shape=x.shape
+        batch,n_fiters,height,width=self.input_shape
         output_height=((height-self.pool)//self.stride) + 1
         output_width=((width-self.pool) // self.stride) + 1
         output=np.zeros((batch,n_fiters,output_height,output_width))
@@ -25,3 +28,34 @@ class average_pooling:
                         
         
         return output
+    
+    def backward(self,d_out):
+        batch,n_kernels,img_height,img_width=d_out.shape
+        output_=np.zeros((self.input_shape))
+        for batch_id, batch in enumerate(d_out):
+            for filter_id, filter in enumerate(batch):
+                for height in range(filter.shape[0]):
+                    for width in range(filter.shape[1]):
+                        start_h=height*stride
+                        start_w=width*stride
+                        output_[batch_id,filter_id,start_h:start_h+self.pool,start_w:start_w+self.pool]=0.25*d_out[batch_id][filter_id][height][width]
+        
+        return output_
+    
+if __name__ == "__main__":
+    x = np.array([[
+        [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9,10,11,12],
+        [13,14,15,16]]
+    ]])
+
+    pool_size=2
+    stride=2
+    print(f"Result before forward pooling \n {x}")
+    pool=average_pooling()
+    x=pool.forward(x)
+    print(f"Result after forward pooling \n {x}")
+    x=pool.backward(x)
+    print(f"Result after backward pooling \n {x}")
+
